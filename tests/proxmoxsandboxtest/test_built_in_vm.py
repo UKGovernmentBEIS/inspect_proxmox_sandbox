1effa47038997875e843a94d6739703091653609
Art.OCathain <Art.OCathain@dsit.gov.uk>
2025-10-21 10:44:20 +0000

    types

--- tests/proxmoxsandboxtest/test_built_in_vm.py
+++ tests/proxmoxsandboxtest/test_built_in_vm.py
@@ -5,13 +5,18 @@ from proxmoxsandbox._impl.qemu_commands import QemuCommands
 async def test_ubuntu(qemu_commands: QemuCommands, built_in_vm: BuiltInVM) -> None:
     await _do_test_builtin(qemu_commands, built_in_vm, "ubuntu24.04")
 
+
 async def test_debian(qemu_commands: QemuCommands, built_in_vm: BuiltInVM) -> None:
     await _do_test_builtin(qemu_commands, built_in_vm, "debian13")
+
 
 async def test_kali(qemu_commands: QemuCommands, built_in_vm: BuiltInVM) -> None:
     await _do_test_builtin(qemu_commands, built_in_vm, "kali2025.3")
 
-async def _do_test_builtin(qemu_commands, built_in_vm, builtin_name):
+
+async def _do_test_builtin(
+    qemu_commands: QemuCommands, built_in_vm: BuiltInVM, builtin_name: str
+):
     await built_in_vm.clear_builtins()
 
     known_builtins = await built_in_vm.known_builtins()
